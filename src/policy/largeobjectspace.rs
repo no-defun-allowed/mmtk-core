21bcb005d8d94ff06a5387b1cfca1a1f1b9a60ec
Kunal Sareen <kunal.sareen@anu.edu.au>
2026-07-09 02:32:16 +0000

    Fix compilation error

--- src/policy/largeobjectspace.rs
+++ src/policy/largeobjectspace.rs
@@ -385,7 +385,7 @@ impl<VM: VMBinding> LargeObjectSpace<VM> {
                 VM::VMObjectModel::GLOBAL_LOG_BIT_SPEC.clear::<VM>(object, Ordering::SeqCst);
             }
             #[cfg(feature = "object_pinning")]
-            VM::VMObjectModel::LOCAL_PINNING_BIT_SPEC.unpin_object::<VM>(object, Ordering::SeqCst);
+            VM::VMObjectModel::LOCAL_PINNING_BIT_SPEC.unpin_object::<VM>(object);
             self.pr
                 .release_pages(get_super_page(object.to_object_start::<VM>()));
         };
